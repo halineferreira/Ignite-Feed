16698a1861ef894d4228a43ca89a33d0aebf9f6c
Haline Ferreira <halineferreira@gmail.com>
2024-01-11 08:12:23 -0300

    Initial structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+import "./styles.css";
 
 function App() {
   return (
     <>
-      <Post />
-      <Post />
+      <Header />
+      <Post author="Haline Silva" content="Bla bla bla" />
+      <Post author="Haline Silva" content="Loren ipsun ..." />
       <Post />
       <Post />
     </>
