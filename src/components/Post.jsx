60788adcfa83967c1a61c284d1ca7b049b6d0161
Haline Ferreira <halineferreira@gmail.com>
2024-01-15 08:28:15 -0300

    Add Post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,34 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/halineferreira.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Haline Ferreira</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="August, 1st at 09:01am" dateTime="2022-08-01 09:01:23">
+          Published 1 hour ago
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>Hi there 👋</p>
+        <p>I'm studying ReactJS to improve my skills 🚀</p>
+        <p>
+          <a href="https://github.com/halineferreira">My Github</a>
+        </p>
+        <p>
+          <a href="">#newProject</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
