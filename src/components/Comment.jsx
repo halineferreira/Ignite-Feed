f4a98a3de92bdc228a566d86543f191c5cf9bebd
Haline Ferreira <halineferreira@gmail.com>
2024-01-16 10:34:36 -0300

    Add Comment Component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/halineferreira.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Haline Ferreira</strong>
+              <time
+                title="August, 1st at 09:01am"
+                dateTime="2022-08-01 09:01:23"
+              >
+                About 1 hour ago
+              </time>
+            </div>
+            <button title="Delete comment">
+              <Trash size="24" />
+            </button>
+          </header>
+          <p>That's great! Congrats! </p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir<span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
