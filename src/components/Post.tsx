f159513678022eae88f1805257f7c11f6ba5bf82
Haline Ferreira <halineferreira@gmail.com>
2024-01-29 23:34:52 -0300

    Update to TypeScript

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,11 +1,28 @@
 import { format, formatDistanceToNow } from "date-fns";
-import { useState } from "react";
+import { FormEvent, InvalidEvent, useState } from "react";
 
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
-export function Post({ author, publishedAt, content }) {
+interface Author {
+  name: string;
+  role: string;
+  avatarUrl: string;
+}
+
+interface Content {
+  type: "paragraph" | "link";
+  content: string;
+}
+
+interface PostProps {
+  author: Author;
+  publishedAt: Date;
+  content: Content[];
+}
+
+export function Post({ author, publishedAt, content }: PostProps) {
   const [comments, setComments] = useState(["Very nice post!"]);
   const [newCommentText, setNewCommentText] = useState("");
 
@@ -18,23 +35,23 @@ export function Post({ author, publishedAt, content }) {
     addSuffix: true,
   });
 
-  function handleCreateNewComment() {
+  function handleCreateNewComment(event: FormEvent) {
     event.preventDefault();
 
     setComments([...comments, newCommentText]);
     setNewCommentText("");
   }
 
-  function handleNewCommentChange() {
+  function handleNewCommentChange(event: FormEvent<HTMLTextAreaElement>) {
     event.target.setCustomValidity("");
     setNewCommentText(event.target.value);
   }
 
-  function handleNewCommentInvalid() {
+  function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
     event.target.setCustomValidity("This is a mandatory field!!!");
   }
 
-  function deleteComment(commentToDelete) {
+  function deleteComment(commentToDelete: string) {
     const commmentsWithoutDeletedOne = comments.filter((comment) => {
       return comment !== commentToDelete;
     });
